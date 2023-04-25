612f096f0e8071bf3c7e0976fda986628fc0c641
Rafał Chojnacki <r.chojnacki2@o2.pl>
2023-04-25 21:01:00 +0200

    HW8 task 1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,51 @@
 // Add imports above this line
+import SimpleLightbox from "simplelightbox";
+console.log(SimpleLightbox);
+// import "simplelightbox/dist/simple-lightbox.min.css";
 import { galleryItems } from './gallery-items';
 // Change code below this line
+const gallery = document.querySelector('.gallery')
+const items = []
+for (const image of galleryItems) {
+  const item = document.createElement('li')
+  item.insertAdjacentHTML("beforeend", `
+  <div class="gallery__item">
+  <a class="gallery__link" href="${image.original}">
+    <img
+      class="gallery__image"
+      src="${image.preview}"
+      data-source="${image.original}"
+      alt="${image.description}"
+    />
+  </a>
+</div>` );
+  items.push(item)
+};
+gallery.append(...items)
 
-console.log(galleryItems);
+
+gallery.addEventListener('click', function(e) {
+  e.preventDefault();
+  
+  if (e.target.tagName === 'IMG') {
+    const imgUrl = e.target.getAttribute('data-source');
+    const instance = basicLightbox.create(`<img src="${imgUrl}" width="100%">`,
+    {
+      onShow: (instance) => {
+        document.addEventListener('keydown', e => {
+          if (e.keyCode !== 27) { return; }
+          
+          instance.close(); 
+        });
+      },
+      onClose: (instance) => {
+        document.removeEventListener('keydown', e => {
+          if (e.keyCode !== 27) { return; }
+          instance.close(); 
+        });
+      }
+    });
+    instance.show()
+  }
+});	
+
