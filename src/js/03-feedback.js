e9b7fd5a049ab14c863ffb25640822be8030ccc6
Rafał Chojnacki <r.chojnacki2@o2.pl>
2023-05-02 01:26:23 +0200

    HW8 all finished

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,17 @@
+const emailInput = document.querySelector('input[type="email"]');
+const messageInput = document.querySelector('textarea[name="message"]');
+const submitButton = document.querySelector('button[type="submit"]');
+setInterval(() => {
+  const emailValue = emailInput.value;
+  const messageValue = messageInput.value;
+  const formData = { email: emailValue, message: messageValue };
+  localStorage.setItem('feedback-form-state', JSON.stringify(formData));
+}, 500);
 
+submitButton.addEventListener('click', (e) => {
+  e.preventDefault();
+  console.log(localStorage.getItem('feedback-form-state'));
+  localStorage.removeItem('feedback-form-state')
+  document.querySelector(".feedback-form").reset();
+  
+})
